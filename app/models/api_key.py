3811db3240846c3f2ed8efd8ff535fd0dfeffa66
Egor PC <yp27062004@mail.ru>
2026-07-10 00:21:40 +0300

    feat: - create ApiKey model

---
+++ app/models/api_key.py
@@ -0,0 +1,19 @@
+from app.config.database import Base
+from datetime import datetime
+from app.models.user import User
+from sqlalchemy.sql import func 
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class ApiKey(Base):
+    __tablename__ = "api_keys"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    user_id : Mapped[int] = mapped_column(ForeignKey("users.id"))
+    provider_name : Mapped[str] = mapped_column(String(50))
+    encrypted_key: Mapped[str] = mapped_column(String(500))
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
+    user: Mapped["User"] = relationship(back_populates="api_keys")
+
+    
