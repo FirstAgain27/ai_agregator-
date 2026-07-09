78b4ffbbd1280ee2957f48898412d3c8c8d105ad
Egor PC <yp27062004@mail.ru>
2026-07-09 23:07:33 +0300

    feat: - add User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,1 +1,18 @@
-from sqlalchemy import Base
+from sqlalchemy import Integer
+
+from app.config.database import Base
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.sql import func
+from datetime import datetime
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
+
